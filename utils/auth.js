5d0e7e7ddec0303f3e1cd70cc1723d5f1ca45b16
janaSunrise <warriordefenderz@gmail.com>
2021-03-22 10:56:31 +0530

    Start integrating with the PayPal SDK.

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,9 @@
-require('dotenv').config({ path: "../" });
+import { clientID, clientSecret } from './env';
 
-const clientID = process.env.PAYPAL_CLIENT_ID;
-const clientSecret = process.env.PAYPAL_CLIENT_SECRET;
-const merchantID = process.env.MERCHANT_ID;
+const checkoutNodeJsSdk = require('@paypal/checkout-server-sdk');
+
+// Change this to `ProductionEnvironment` when it's live.
+const environment = new checkoutNodeJsSdk.core.SandboxEnvironment(clientID, clientSecret);
+const client = new checkoutNodeJsSdk.core.PayPalHttpClient(environment);
+
+module.exports = {client: client};
---
+++ utils/utils.js
@@ -0,0 +1,27 @@
+async function prettyPrint(jsonData, pre=""){
+    let pretty = "";
+
+    function capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    }
+
+    for (let key in jsonData){
+        if (jsonData.hasOwnProperty(key)){
+            if (isNaN(key))
+                pretty += pre + capitalize(key) + ": ";
+            else
+                pretty += pre + (parseInt(key) + 1) + ": ";
+            if (typeof jsonData[key] === "object"){
+                pretty += "\n";
+                pretty += await prettyPrint(jsonData[key], pre + "    ");
+            }
+            else {
+                pretty += jsonData[key] + "\n";
+            }
+
+        }
+    }
+    return pretty;
+}
+
+module.exports = { prettyPrint: prettyPrint };
